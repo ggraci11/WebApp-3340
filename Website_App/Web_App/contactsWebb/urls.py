4e98b252682a7f5f8743742555b280d46981a5ff
Miguel Martinez <miguel.martinez17@utrgv.edu>
2025-04-28 14:20:01 -0500

    Updated login_register file

--- Website_App/Web_App/contactsWebb/urls.py
+++ Website_App/Web_App/contactsWebb/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('',views.home, name = 'home'),
+    path('login/', views.login_view, name='login'),
+    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
+
 ]
--- Website_App/Web_App/contactsWebb/views.py
+++ Website_App/Web_App/contactsWebb/views.py
@@ -1,6 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Task
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
 	return render(request,"home.html",{})
+
+def home(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+
+        return redirect('home')  # redirect to avoid re-submitting the form
+
+    tasks = Task.objects.all()
+    return render(request, 'home.html', {'tasks': tasks})
+
+def delete_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    task.delete()
+    return redirect('home')
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def register_view(request):
+    return render(request, 'register.html')
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('/')  # redirect to home or dashboard
+        else:
+            messages.error(request, 'Invalid username or password')
+
+    return render(request, 'login.html')
